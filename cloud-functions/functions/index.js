27b3e566b61b4b33761e898a1d130d8285a81d3c
Aleksander Parelo <aleksander@parelo.ee>
2019-12-18 09:00:01 +0200

    Added second door

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -11,9 +11,10 @@ exports.doorChanged = functions.database.ref('/doors/VhNuY5pnirysCIzhZkjT/isOpen
         var payload = {
             data:{
               doorSate: original,
+              doorName: "Front Door",
               registration_ids: ["e0qcsEJsGDQ:APA91bFgyBu2EsnEbq1_vMCKyT3-AoFIbxBBehC7GZXPsJNXd3sHWqFZXVC8hXMXhKsI1JmLKxb19tp6XlvDXJj-lb7O1HVmBosUgqQN2ggPhDxgbg2fimECLKxJc6gNurfvqUjrGw7E"]
             }
         };
         console.log("Sending message");
-        return admin.messaging().sendToDevice("1:921772010269:android:2b942693d4e2987d4d4ff4", payload);
+        return admin.messaging().sendToDevice("e0qcsEJsGDQ:APA91bFgyBu2EsnEbq1_vMCKyT3-AoFIbxBBehC7GZXPsJNXd3sHWqFZXVC8hXMXhKsI1JmLKxb19tp6XlvDXJj-lb7O1HVmBosUgqQN2ggPhDxgbg2fimECLKxJc6gNurfvqUjrGw7E", payload);
     })
